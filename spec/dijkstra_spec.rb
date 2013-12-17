473994f75006840ce60cd9793634e1c3c65dacd4
Gilberto Villa <ingilniero@gmail.com>
2013-12-16 23:58:47 -0600

    Update dijkstra spec adding a scenario

--- spec/dijkstra_spec.rb
+++ spec/dijkstra_spec.rb
@@ -4,9 +4,27 @@ require 'lib/node.rb'
 
 describe Dijkstra::Algorithm do
   let(:node_a) { Dijkstra::Node.new 'a' }
-  let(:node_h) { Dijkstra::Node.new 'h' }
+  let(:node_b) { Dijkstra::Node.new 'b' }
+  let(:node_c) { Dijkstra::Node.new 'c' }
+  let(:node_d) { Dijkstra::Node.new 'd' }
+  let(:node_e) { Dijkstra::Node.new 'e' }
+  let(:node_f) { Dijkstra::Node.new 'f' }
+
+  let(:graph)  { Dijkstra::Graph.new }
+
+  before do
+    graph.link(node_a, node_b, 7)
+    graph.link(node_a, node_c, 9)
+    graph.link(node_b, node_c, 10)
+    graph.link(node_b, node_d, 15)
+    graph.link(node_a, node_f, 14)
+    graph.link(node_c, node_f, 2)
+    graph.link(node_c, node_g, 11)
+    graph.link(node_f, node_e, 9)
+    graph.link(node_d, node_e, 6)
+  end
 
   it 'finds the shortest path for a given node pair' do
-    expect(subject.shortest_path_for(node_a, node_h)).to be_true
+    expect(subject.shortest_path_for(node_a, node_f)).to be_true
   end
 end
