49dbdde7bc4a142f12e794d54a43f63733f312ce
Gilberto Villa <ingilniero@gmail.com>
2013-12-16 23:46:30 -0600

    Update Node namespace

--- spec/dijkstra_spec.rb
+++ spec/dijkstra_spec.rb
@@ -1,9 +1,9 @@
 require 'lib/dijkstra.rb'
 
-describe Dijkstra::Algorithm do
-  let(:node_a) { Node.new 'a' }
-  let(:node_h) { Node.new 'h' }
 
+describe Dijkstra::Algorithm do
+  let(:node_a) { Dijkstra::Node.new 'a' }
+  let(:node_h) { Dijkstra::Node.new 'h' }
 
   it 'finds the shortest path for a given node pair' do
     subject.shortest_path_for(node_a, node_h)
