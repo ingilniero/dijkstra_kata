9628065fcf46d4a0af0aeac2e17d31908cd45aa0
Gilberto Villa <ingilniero@gmail.com>
2013-12-31 13:17:05 -0600

    Visit a node and mark it as visited

--- lib/dijkstra.rb
+++ lib/dijkstra.rb
@@ -2,6 +2,14 @@ module Dijkstra
   class Algorithm
     def shortest_path_for(source_node, end_node)
       source_node.weight = 0
+      visit(source_node, end_node)
+    end
+
+    private
+
+    def visit(node, end_node)
+      return if node == end_node
+      node.mark_as_visited
     end
   end
 end
