c769fa99a862c49d2a933afd5fa9b187a8124f53
Gilberto Villa <ingilniero@gmail.com>
2013-12-31 11:26:34 -0600

    Update expectation for link method

--- spec/graph_spec.rb
+++ spec/graph_spec.rb
@@ -1,8 +1,17 @@
 require 'lib/graph.rb'
 
 describe Dijkstra::Graph do
-  it 'has a link method' do
-    expect(subject.respond_to?(:link)).to be_true
+  describe '#link' do
+    let(:node_a) { Dijkstra::Node.new 'a' }
+    let(:node_b) { Dijkstra::Node.new 'b' }
+
+    before do
+      subject.link(node_a, node_b, 5)
+    end
+
+    it 'adds node "b" to the neighbours of node "a" as a neighbour object' do
+      expect(node_a.neighbours.map(&:node)).to eq [ node_b ]
+    end
   end
 
 end
