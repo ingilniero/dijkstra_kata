9dd5b120e411526c05e8ee6e12591efcc453ab6b
Gilberto Villa <ingilniero@gmail.com>
2013-12-31 11:30:35 -0600

    Add second node as neighbour of the first node

--- lib/graph.rb
+++ lib/graph.rb
@@ -3,6 +3,7 @@ module Dijkstra
 
     def link(node_a, node_b, distance)
         node_a.neighbours << Dijkstra::Neighbour.new(node_b, distance)
+        node_b.neighbours << Dijkstra::Neighbour.new(node_a, distance)
     end
   end
 end
