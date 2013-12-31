dfb9b018671a2106aa6a1cb1e2343870738e83e8
Gilberto Villa <ingilniero@gmail.com>
2013-12-31 11:05:35 -0600

    Nodes have an array of neighbours

--- lib/node.rb
+++ lib/node.rb
@@ -1,7 +1,7 @@
 module Dijkstra
   class Node
     attr_reader :name
-    attr_accessor :weight, :visited
+    attr_accessor :weight, :visited, :neighbours
 
     INFINITY = 1000
 
@@ -9,6 +9,7 @@ module Dijkstra
       @name = name
       @weight = INFINITY
       @visited = false
+      @neighbours = []
     end
 
   end
