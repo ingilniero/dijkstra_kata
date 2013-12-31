f87773d8bd2628f8589ad96d9e55fa1e796a4c6b
Gilberto Villa <ingilniero@gmail.com>
2013-12-31 10:37:29 -0600

    Nodes are unvisited as initial state

--- lib/node.rb
+++ lib/node.rb
@@ -1,13 +1,14 @@
 module Dijkstra
   class Node
     attr_reader :name
-    attr_accessor :weight
+    attr_accessor :weight, :visited
 
     INFINITY = 1000
 
     def initialize(name)
       @name = name
       @weight = INFINITY
+      @visited = false
     end
 
   end
